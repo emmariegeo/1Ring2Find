9efbb45debaa3609f080403d62380cb08de45039
emmariegeo <57061691+emmariegeo@users.noreply.github.com>
2023-07-22 12:35:42 -0700

    Update listener

--- index.js
+++ index.js
@@ -170,8 +170,8 @@ app.get('/character', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
 
 
